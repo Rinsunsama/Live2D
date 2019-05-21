212424b6aac846ced50c6bbb519b2cf1a2058095
rinsun <396438398@qq.com>
2019-05-21 17:06:51 +0800

    init

---
+++ Assets/Scripts/Live2DModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using live2d;
+
+public class Live2DModel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Live2D.init();
+        Live2DModelUnity asset = Live2DModelUnity.loadModel(Application.dataPath + "/Resources/Epsilon/runtime/Epsilon.moc");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
